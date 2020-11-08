65f0fff40e6597a9f4ebe0bd6bdd8b6ab7c99b5b
Vyl-05 <47647606+Vyl-05@users.noreply.github.com>
2020-11-08 18:04:13 -0500

    Player Movement
    Created a new folder called "Sprites" and added a diamond sprite names "TempPlayer"
    
    created a new empty object and called it "Player" .  Gave the player a Rigidbody2D and made a new script called "PlayerMovement" and added it into the Scripts folder.
    
    Added a new constant to the consatnts script called "moveSpeed"

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private float moveSpeed;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        moveSpeed = Constants.moveSpeed;
+    }
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
