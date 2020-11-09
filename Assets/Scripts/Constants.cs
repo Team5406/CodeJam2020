936ab15afd7ab30f5660d4313256e295f931cb4b
ChristianFemia <chriseth45@gmail.com>
2020-11-08 21:45:23 -0500

    Added a Title screen
    Added a title screen, that has a working Play, Options, and quit button.
    
    The Options panel has the ability to request the users computers resoultion, and create options for them to work. There is also a full screen, and graphics option button, which also do work. Finally their is a working volume slider, that does change the master volume settings.

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,4 +3,15 @@
     
     public const float moveSpeed = 5f;
 
+
+
+    //Audio Mixer Names
+    public const string masterAudioMixer = "volume";
+
+    //Scene Indexs
+    public const int titleScreenIndex = 0;
+    public const int tradingSceneIndex = 1;
+    public const int scavengerHuntIndex = 2;
+    public const int endScreenIndex = 3;
+
 }
