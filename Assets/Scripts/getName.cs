a9ec488aa1099a2807c205b196324755348d9376
ChristianFemia <chriseth45@gmail.com>
2020-11-11 20:49:37 -0500

    Added the ability to enter a username.
    Username will be seen on the header bar, which is to be implimented soon.

---
+++ Assets/Scripts/getName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getName : MonoBehaviour
+{
+    public GameObject nameInputField;
+
+
+    public string playerName;
+    public void storeName()
+    {
+        playerName = nameInputField.GetComponent<Text>().text;
+        PlayerPrefs.SetString("Name", playerName);
+        Debug.Log(playerName);
+
+    }
+}
+
