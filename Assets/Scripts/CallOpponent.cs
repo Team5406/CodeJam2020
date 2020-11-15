538505d1fc99930c4c59c6a8fd6e589d81476ead
ChristianFemia <chriseth45@gmail.com>
2020-11-15 17:31:51 -0500

    wrote some inventory code

--- Assets/Scripts/CallOpponent.cs
+++ Assets/Scripts/CallOpponent.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CallOpponent : MonoBehaviour
 {
+
+    TextMeshProUGUI userItem;
+    TextMeshProUGUI opponentItem;
     // Start is called before the first frame update
     void Start()
     {
         InventoryManagment.calculateOpponentInventory();
         string[] trades = InventoryManagment.pickItemsToTrade();
+        userItem.GetComponent<TextMeshProUGUI>().text = "Would you like to trade " + trades[1] + " for " + trades[2] + "."
     }
 
     // Update is called once per frame
