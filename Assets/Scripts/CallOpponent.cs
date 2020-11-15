fed0fc6e71ceffcad1df25aef0fa9397e2d63012
ChristianFemia <chriseth45@gmail.com>
2020-11-15 17:46:30 -0500

    did some more text changes

--- Assets/Scripts/CallOpponent.cs
+++ Assets/Scripts/CallOpponent.cs
@@ -6,14 +6,13 @@ using TMPro;
 public class CallOpponent : MonoBehaviour
 {
 
-    TextMeshProUGUI userItem;
-    TextMeshProUGUI opponentItem;
+    public TextMeshProUGUI tradeText;
     // Start is called before the first frame update
     void Start()
     {
         InventoryManagment.calculateOpponentInventory();
         string[] trades = InventoryManagment.pickItemsToTrade();
-        userItem.GetComponent<TextMeshProUGUI>().text = "Would you like to trade " + trades[1] + " for " + trades[2] + "."
+        tradeText.GetComponent<TextMeshProUGUI>().text = "Would you like to trade " + trades[0] + " for " + trades[1] + ".";
     }
 
     // Update is called once per frame
